00ca9b5383a01e765124a156bd09fce142a66f66
Patrick Wawrzoszek <patrick.wawrzoszek@hotmail.com>
2021-01-10 18:11:20 -0500

    feat: Selecting a single transaction, navigating through popups and forms and splitting successfully.

--- splitter.py
+++ splitter.py
@@ -29,10 +29,19 @@ try:
 except:
     print('Cannot find transaction element')
 
+
+
+sample_txn = mint.driver.find_element_by_id("transaction-1368494644")
 time.sleep(1)
+sample_txn.click()
 
+txn_edit_toggle = mint.driver.find_element_by_id("txnEdit-toggle")
+time.sleep(1)
+txn_edit_toggle.click()
 
-
+txn_split_bttn = mint.driver.find_element_by_id("txnEdit-split")
+time.sleep(1)
+txn_split_bttn.click()
 
 txns = mint.get_transactions_json(id=joint_card["id"], start_date="1 December 2020")
 # isChild field determines if it's joint or not
