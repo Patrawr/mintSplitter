3986576b48bc2c6853bf4304b13e5c8e9e086133
Patrick Wawrzoszek <patrick.wawrzoszek@hotmail.com>
2021-01-17 23:25:02 -0500

    feat: Script can now successfully split  a single transaciton using sendKeys and macOS commands. Need to enable for windows and linux.

--- splitter.py
+++ splitter.py
@@ -3,6 +3,7 @@ import keyring
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 print ('hello')
@@ -30,21 +31,49 @@ except:
     print('Cannot find transaction element')
 
 
-
+#find a specific transaction in list on transactions page and click on it
 sample_txn = mint.driver.find_element_by_id("transaction-1368494644")
 time.sleep(1)
 sample_txn.click()
 
+# once transaction is selected, click on toggle to bring up txn edit modal
 txn_edit_toggle = mint.driver.find_element_by_id("txnEdit-toggle")
 time.sleep(1)
 txn_edit_toggle.click()
 
+# click on the split button in the txn edit modal
 txn_split_bttn = mint.driver.find_element_by_id("txnEdit-split")
 time.sleep(1)
 txn_split_bttn.click()
 
+# within the split your transaction pop-up, this is how the two editable category and amount fields are returned
+split_txn_table_categories = mint.driver.find_elements_by_xpath(
+            "//*[@id='pop-split-main-table']/tbody/tr[not(@class='hide')]/td[@class='category-cell']/input[@name='category']")
+
+split_txn_table_categories[0].clear()
+
+split_txn_table_categories[0].send_keys(Keys.COMMAND + "a") 
+split_txn_table_categories[0].send_keys(Keys.DELETE)
+split_txn_table_categories[0].send_keys("Hide from Budgets & Trends")
+
+#get input elements for amount fields in transaction pop-up
+split_txn_table_amounts = mint.driver.find_elements_by_xpath(
+            "//*[@id='pop-split-main-table']/tbody/tr[not(@class='hide')]/td[@class='amount-cell']/span/input")
+
+split_txn_table_amounts[0].send_keys(Keys.COMMAND + "a")
+split_txn_table_amounts[0].send_keys(Keys.DELETE)
+split_txn_table_amounts[0].send_keys("12.00")
+
+split_txn_table_amounts[1].send_keys(Keys.COMMAND + "a")
+split_txn_table_amounts[1].send_keys(Keys.DELETE)
+split_txn_table_amounts[1].send_keys("13.82")
+
+split_submit_button = mint.driver.find_element_by_id("pop-split-submit")
+split_submit_button.click()
+
 txns = mint.get_transactions_json(id=joint_card["id"], start_date="1 December 2020")
 # isChild field determines if it's joint or not
+
 
 
 joint_txns=[]
