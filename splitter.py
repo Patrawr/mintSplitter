8f8da2edff8bd2ef94a8cc2a53d2139d7983dbd9
Patrick Wawrzoszek <patrick.wawrzoszek@hotmail.com>
2021-02-04 01:19:02 -0500

    feat: added filtering out not yet split joint txns. Will loop through and split any unplit transactions. Start date also works correctly now.

--- splitter.py
+++ splitter.py
@@ -1,6 +1,7 @@
 import mintapi
 import keyring
 import time
+from datetime import date, datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -20,13 +21,16 @@ def locate_and_split_txn(driver, transaction_id, total_amount):
     time.sleep(1)
     #find a specific transaction in list on transactions page and click on it
     try: 
-        sample_txn = WebDriverWait(driver, 10).until(
+        txn_row = WebDriverWait(driver, timeout=10).until(
             EC.element_to_be_clickable((By.ID, f"transaction-{transaction_id}"))
         )
     except:
-        print('Cannot find transaction element')
+        print('Cannot find transaction list element')
     else:    
-        sample_txn.click()
+        print('Found txn row to click')
+        driver.execute_script("arguments[0].scrollIntoView();", txn_row)
+        driver.execute_script("arguments[0].click();", txn_row)
+        # txn_row.click()
 
     time.sleep(0.5)
     # once transaction is selected, click on toggle to bring up txn edit modal
@@ -50,7 +54,7 @@ def locate_and_split_txn(driver, transaction_id, total_amount):
     split_txn_table_categories = driver.find_elements_by_xpath(
                 "//*[@id='pop-split-main-table']/tbody/tr[not(@class='hide')]/td[@class='category-cell']/input[@name='category']")
 
-
+    time.sleep(0.3)
     split_txn_table_categories[0].send_keys(Keys.COMMAND + "a") 
     split_txn_table_categories[0].send_keys(Keys.DELETE)
     split_txn_table_categories[0].send_keys("Hide from Budgets & Trends")
@@ -78,6 +82,8 @@ print ('hello')
 username = keyring.get_password("mint_splitter", "user")
 pw = keyring.get_password("mint_splitter", "password")
 
+test_string_amount = "$14.50"
+cast_float_amount = float(test_string_amount.replace('$', ''))
 
 mint = mintapi.Mint(
     username,
@@ -99,21 +105,21 @@ try:
 except:
     print('Cannot find transactions for account: $$$$')
 
-time.sleep(3)
 
-locate_and_split_txn(mint.driver, "1368494644", 25.82)
-locate_and_split_txn(mint.driver, "1369145342", 45.61)
+# isChild field determines if it's joint or not, date is mm/dd/yy format
+txns = mint.get_transactions_json(id=joint_card["id"], start_date="01/01/21")
 
-# isChild field determines if it's joint or not
-txns = mint.get_transactions_json(id=joint_card["id"], start_date="1 December 2020")
-
+time.sleep(5)
 
 joint_txns=[]
 
 for txn in txns:
-    if txn["isChild"]:
+    if  not txn["isChild"] and txn["category"] != "Hide from Budgets & Trends" and txn["isDebit"] and not txn["isPending"]:
         joint_txns.append(txn)
-        print (txn)
-
-
-print (joint_txns)
+        time.sleep(1)
+        locate_and_split_txn(mint.driver, str(txn["id"]), float(txn["amount"].replace('$', '')))
+        
+# convert amount from string in $9.99 format to float
+# locate_and_split_txn(mint.driver, str(joint_txns[0]["id"]), float(joint_txns[0]["amount"].replace('$', '')))
+locate_and_split_txn(mint.driver, "1368494644", 25.82)
+locate_and_split_txn(mint.driver, "1369145342", 45.61)
