e4bfd41cf4a3cf6b15ccf4f5b6492d28d63cabfb
Patrick Wawrzoszek <patrick.wawrzoszek@hotmail.com>
2021-02-11 00:16:05 -0500

    feat: Cleaning up older methods.

--- splitter.py
+++ splitter.py
@@ -9,104 +9,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+# takes in a monetary amount, strips off the $ and splits into two equal floats,
+# with 2 decimal precision
 def calculate_split(total_amount):
     splits = []
+    stripped_amount = float(total_amount.replace('$', ''))
 
-    splits.append(round(total_amount / 2, 2))
-    splits.append(round(total_amount - splits[0], 2))
+    splits.append(round(stripped_amount / 2, 2))
+    splits.append(round(stripped_amount - splits[0], 2))
     return splits
 
 
-def locate_and_split_txn(driver, transaction_id, total_amount):
-    time.sleep(1)
-    #find a specific transaction in list on transactions page and click on it
-    try: 
-        txn_row = WebDriverWait(driver, timeout=10).until(
-            EC.element_to_be_clickable((By.ID, f"transaction-{transaction_id}"))
-        )
-    except:
-        print('Cannot find transaction list element')
-    else:    
-        print('Found txn row to click')
-        driver.execute_script("arguments[0].scrollIntoView();", txn_row)
-        driver.execute_script("arguments[0].click();", txn_row)
-        # txn_row.click()
-
-    time.sleep(0.5)
-    # once transaction is selected, click on toggle to bring up txn edit modal
-    txn_edit_toggle = driver.find_element_by_id("txnEdit-toggle")
-
-    txn_edit_toggle.click()
-
-    time.sleep(0.5)
-    # click on the split button in the txn edit modal once the modal is loaded
-    try: 
-        txn_split_bttn = driver.find_element_by_id("txnEdit-split")
-        WebDriverWait(driver, 10).until(
-                lambda d : "expense hide" not in d.find_element_by_id("txnEdit-form").get_attribute('class')
-            )
-    except:
-        print('Cannot find split button element')
-    else:
-        txn_split_bttn.click()
-
-    # within the split your transaction pop-up, this is how the two editable category and amount fields are returned
-    split_txn_table_categories = driver.find_elements_by_xpath(
-                "//*[@id='pop-split-main-table']/tbody/tr[not(@class='hide')]/td[@class='category-cell']/input[@name='category']")
-
-    time.sleep(0.3)
-    split_txn_table_categories[0].send_keys(Keys.COMMAND + "a") 
-    split_txn_table_categories[0].send_keys(Keys.DELETE)
-    split_txn_table_categories[0].send_keys("Hide from Budgets & Trends")
-
-    #get input elements for amount fields in transaction pop-up
-    split_txn_table_amounts = driver.find_elements_by_xpath(
-                "//*[@id='pop-split-main-table']/tbody/tr[not(@class='hide')]/td[@class='amount-cell']/span/input")
-
-    # figure out what the actual two even split amounts will be
-    split_amounts = calculate_split(total_amount)
-
-    split_txn_table_amounts[0].send_keys(Keys.COMMAND + "a")
-    split_txn_table_amounts[0].send_keys(Keys.DELETE)
-    split_txn_table_amounts[0].send_keys(f"{split_amounts[0]}")
-
-    split_txn_table_amounts[1].send_keys(Keys.COMMAND + "a")
-    split_txn_table_amounts[1].send_keys(Keys.DELETE)
-    split_txn_table_amounts[1].send_keys(f"{split_amounts[1]}")
-
-    split_submit_button = driver.find_element_by_id("pop-split-submit")
-    split_submit_button.click()
-
-def submit_split_form(mint, txn_id):
+def submit_split_form(mint, txn):
     url = 'https://mint.intuit.com/updateTransaction.xevent'
+
+    split_amounts = calculate_split(txn['amount'])
+
     data = {'task': 'split',
             'data': '', 
-            'txnId': f"{txn_id}:0", 
+            'txnId': f"{txn['id']}:0",
             'token': mint.token,
-            'amount0': 79.39,
-            'category0': 'Groceries',
-            'merchant0': 'Farm Boy',
+            'amount0': split_amounts[0],
+            'category0': txn['category'],
+            'merchant0': txn['merchant'],
             'txnId0': 0,
-            'percentAmount0': 79.39,
-            'categoryId0': 701,
-            'amount1': 100,
+            'percentAmount0': split_amounts[0],
+            'categoryId0': txn['categoryId'],
+            'amount1': split_amounts[1],
             'category1': 'Hide from Budgets & Trends',
-            'merchant1': 'Farm Boy',
+            'merchant1': txn['merchant'],
             'txnId1': 0,
-            'percentAmount1': 100,
+            'percentAmount1': split_amounts[1],
+            # category for hide from budgets and trends
             'categoryId1': 40
             }
 
-    headers = {'accept': '*/*',
-               'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-               'origin': 'https://mint.intuit.com',
-               'referer': 'https://mint.intuit.com/transaction.event',
-               'dnt': '1'
-               }
-
     result = mint.post(url, data=data)
 
-    print(result.text)
+    print(f"Split {txn['merchant']} for {txn['amount']} on {txn['date']}")
 
 # start of script #############################################################################################
 print ('Starting splitter')
@@ -140,20 +80,13 @@ except:
 
 
 # isChild field determines if it's joint or not, date is mm/dd/yy format
-txns = mint.get_transactions_json(id=joint_card["id"], start_date="01/01/21")
+txns = mint.get_transactions_json(id=joint_card["id"], start_date="12/24/20")
 print(f"Retrieved {len(txns)} txns for {joint_card['accountName']}")
 
-submit_split_form(mint, "1376006654")
 
 joint_txns=[]
 
 for txn in txns:
     if  not txn["isChild"] and txn["category"] != "Hide from Budgets & Trends" and txn["isDebit"] and not txn["isPending"]:
         joint_txns.append(txn)
-        time.sleep(1)
-        locate_and_split_txn(mint.driver, str(txn["id"]), float(txn["amount"].replace('$', '')))
-        
-# convert amount from string in $9.99 format to float
-# locate_and_split_txn(mint.driver, str(joint_txns[0]["id"]), float(joint_txns[0]["amount"].replace('$', '')))
-# locate_and_split_txn(mint.driver, "1368494644", 25.82)
-# locate_and_split_txn(mint.driver, "1369145342", 45.61)
+        submit_split_form(mint, txn)
